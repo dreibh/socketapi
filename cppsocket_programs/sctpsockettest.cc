4775f6da6864d6642e819a3169876e70d1f43f00
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2015-03-01 16:06:41 +0000

    Updated socketapi to latest build scripts. New release.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/socketapi@2838 516812f1-d3fd-0310-9ea4-9aededebcb87

--- cppsocket_programs/sctpsockettest.cc
+++ cppsocket_programs/sctpsockettest.cc
@@ -2,7 +2,7 @@
  *  $Id$
  *
  * SocketAPI implementation for the sctplib.
- * Copyright (C) 2005-2013 by Thomas Dreibholz
+ * Copyright (C) 2005-2015 by Thomas Dreibholz
  *
  * Realized in co-operation between
  * - Siemens AG
@@ -619,7 +619,7 @@ int main(int argc, char** argv)
 
 
    // ====== Print information ==============================================
-   std::cout << "SCTP Socket Test - Copyright (C) 2013 Thomas Dreibholz" << std::endl;
+   std::cout << "SCTP Socket Test - Copyright (C) 2015 Thomas Dreibholz" << std::endl;
    std::cout << "------------------------------------------------------" << std::endl;
    std::cout << "Version:           " << __DATE__ << ", " << __TIME__ << std::endl;
    std::cout << "Server Mode:       " << ((server == true) ? "on" : "off") << std::endl;
